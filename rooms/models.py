53cc5c25347f64c8084e2f7bc0862258b5276802
Naz <171716584+nazbur05@users.noreply.github.com>
2026-04-23 10:12:06 +0500

    fix: remove timezone field from Building model (because there is no need)

--- rooms/models.py
+++ rooms/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Building(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    timezone = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Room(models.Model):
