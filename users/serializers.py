250556253be25370f5aba625611157bfae3db445
Naz <171716584+nazbur05@users.noreply.github.com>
2026-04-30 01:03:49 +0500

    feat: update user model to use UUID for primary key and enforce unique email; adjust serializers to reflect changes

--- users/serializers.py
+++ users/serializers.py
@@ -9,7 +9,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email', 'role')
+        fields = ('username', 'password', 'email')
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
@@ -18,4 +18,4 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'role')
+        fields = ('username', 'email')
